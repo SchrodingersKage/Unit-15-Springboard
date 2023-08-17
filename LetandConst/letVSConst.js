c14d8c7c27f099674e209d2181894653e65b255c
daxkharris <daxkharris@pop-os.localdomain>
2023-08-16 19:35:35 -0600

    Changed organization structure

---
+++ LetandConst/letVSConst.js
@@ -0,0 +1,12 @@
+const PI = 3.14
+
+/*
+Var is different from Let because let will not allow you to redeclare that variable. As well as
+Let is block scope, and var is function scope.
+
+Var and const are different because const is a value that can not be changed, As well as it has
+block scope like let.
+
+Let and const are different because let allows you to redefine the value in the variable. While
+const doesn't.
+*/
