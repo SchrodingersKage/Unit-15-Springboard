02025d2a6615d703a96e40e114f942377dad1667
daxkharris <daxkharris@pop-os.localdomain>
2023-08-20 10:44:47 -0600

    Finished up rest and spread assignment

--- RestandSpread/restAndSpread.js
+++ RestandSpread/restAndSpread.js
@@ -12,3 +12,25 @@ const mergeObjects = (object1, object2) => {
 const doubleAndReturnArgs = (arr, ...args) => {
     return newArr = [ ...arr, ...args.map(v => v * 2)]
 }
+
+
+const removeRandom = (items) => {
+    let index = Math.floor(Math.random() * items.length)
+    return [ ...items.slice(0, index), ...items.slice(index + 1, items.length)]
+}
+
+const extend = (array1, array2) => [...array1, ...array2]
+
+const addKeyVal = (obj, key, val) => ({ ...obj, [key]: val })
+
+
+const removeKey = (obj, key) {
+    const newObj = { ...obj }
+    delete newObj[key];
+    return newObj;
+}
+
+const combine = (obj1, obj2) => ({ ...obj1, ...obj2})
+
+const update = (obj, key, val) => ({ ...obj, [key]: val});
+
